45f1299469faa44106740cc8f5962375d8926e7f
SimG <giraudsimeon@gmail.com>
2026-06-06 00:42:30 +0200

    update for vercel deployment (fix)

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,5 +1,6 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'dummy_key', {
   apiVersion: '2026-02-25.preview' as any,
 })
+
